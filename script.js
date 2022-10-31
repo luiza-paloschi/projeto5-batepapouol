25c735f257c11c47f34ff47434451553206e5436
luiza-paloschi <luizapaloschi@gmail.com>
2022-10-31 08:01:42 -0300

    versão final com bônus 1 e 2

--- script.js
+++ script.js
@@ -1,89 +1,81 @@
 let messages = [];
 let ultimaMensagem;
-let usuário = {}
-let participantes = []
+let usuário = {};
+let participantes = [];
 let destinatario;
 let tipo;
-function fazerLogin(){
-    const nome = document.querySelector('.user').value
-    if(nome !== ''){
-        usuário = {name: nome}
-       const entrar = document.querySelector('.entrar')
-        entrar.innerHTML = 
-        `<img class ="gif" src ="./images/loading2.gif"/>
-        <div>Entrando</div>`
-    
-        const aa =  axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', usuário)
-       aa.then(entrarnaSala)
-        aa.catch(erroAutenticacao)
-    } else{
-        alert('Por favor, digite um nome!') 
-    }
 
+function fazerLogin(){
+    const nome = document.querySelector('.user').value;
+    if(nome !== ''){
+        usuário = {name: nome};
+        const entrar = document.querySelector('.entrar');
+        entrar.innerHTML =
+        `<img class ="gif" alt="carregando" src ="./images/loading2.gif"/>
+        <div>Entrando</div>`;
+        const enviarNome =  axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', usuário);
+        enviarNome.then(entrarnaSala);
+        enviarNome.catch(erroAutenticacao);
+    } else if(nome === ''){
+        alert('Por favor, digite um nome!');
+    }
 }
 
 function erroAutenticacao(erro){
     console.log("Status code: " + erro.response.status);
-	alert('Esse nome já está em uso! Digite outro nome.')
-    window.location.reload()
-    
-} 
+    console.log(nome)
+	alert('Esse nome já está em uso! Digite outro nome.');
+    window.location.reload(); 
+}
 
 function entrarnaSala(){
-    recebeMensagens()
-    manterConexão()
-    pegaParticipantes()
-    usuarioLogado()
-    selTodos()
-    const telaLogin = document.querySelector('.tela_login')
-    telaLogin.classList.add('hidden')
-    const conteudo = document.querySelectorAll('.content')
+    recebeMensagens();
+    manterConexão();
+    pegaParticipantes();
+    usuarioLogado();
+    selTodos();
+    const telaLogin = document.querySelector('.tela_login');
+    telaLogin.classList.add('hidden');
+    const conteudo = document.querySelectorAll('.content');
     for(let i = 0; i< conteudo.length; i++){
-        conteudo[i].classList.remove('hidden')
+        conteudo[i].classList.remove('hidden');
     }
 }
 
 function usuarioLogado(){
-    setInterval(manterConexão, 5000)
-    setInterval(recebeMensagens, 3000)
-    setInterval(pegaParticipantes, 6000)
+    setInterval(manterConexão, 5000);
+    setInterval(recebeMensagens, 3000);
+    setInterval(pegaParticipantes, 6000);
 }
 
 function manterConexão(){
-    const resposta =axios.post('https://mock-api.driven.com.br/api/v6/uol/status', usuário)
-    resposta.catch(erroConexao)
+    const resposta =axios.post('https://mock-api.driven.com.br/api/v6/uol/status', usuário);
+    resposta.catch(erroConexao);
 }
 function erroConexao(erro){
-    alert("Erro de conexão. Por favor, entre novamente")
-    window.location.reload()
+    alert("Erro de conexão. Por favor, entre novamente.");
+    window.location.reload();
 }
 
 function recebeMensagens(){
     const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    promise.then(respostaMensagens)
-
-}
-
-function respostaMensagens(resposta){
-    messages = resposta.data
-    renderizarMensagens()
-    
+    promise.then( (resposta) => {
+        messages = resposta.data;
+        renderizarMensagens();
+    })
 }
 
 function renderizarMensagens(){
-
-    const chat = document.querySelector('.messages')
-    chat.innerHTML = ''
+    const chat = document.querySelector('.messages');
+    chat.innerHTML = '';
     for(let i = 0; i < messages.length; i++){
-      
         if(messages[i].type === 'status'){
-            chat.innerHTML += 
+            chat.innerHTML +=
             `<div class = "mensagem status">
                 <span class = "time">(${messages[i].time})&nbsp;</span>
                 <b>${messages[i].from}&nbsp;</b>
                 <span class="text">${messages[i].text}&nbsp;</span>
-            </div>`
-      
+            </div>`;
         } else if (messages[i].type === 'message'){
             chat.innerHTML +=
              `<div class = "mensagem message">
@@ -91,8 +83,7 @@ function renderizarMensagens(){
                 <b> ${messages[i].from}&nbsp;</b>
                 <div>para&nbsp<b>${messages[i].to}</b>:&nbsp</div>
                 <div class="text">${messages[i].text}&nbsp</div>
-              </div>`
-            
+              </div>`;
         } else if (messages[i].type === 'private_message'){
                 if(messages[i].to === usuário.name || messages[i].from === usuário.name){
                     chat.innerHTML +=
@@ -101,174 +92,176 @@ function renderizarMensagens(){
                        <b> ${messages[i].from}&nbsp;</b>
                        <div>reservadamente para&nbsp<b>${messages[i].to}</b>:&nbsp</div>
                        <div class="text">${messages[i].text}&nbsp</div>
-                     </div>` 
+                     </div>`;
                 }
         }
-      
-        
     }
     Scroll();
 }
 
 function Scroll(){
-    const mensagens = document.querySelector(".messages")
+    const mensagens = document.querySelector(".messages");
     const novaMensagem = mensagens.lastElementChild;
-    const html = novaMensagem.innerHTML
+    const html = novaMensagem.innerHTML;
     if(ultimaMensagem !== html){
         ultimaMensagem = html;
         novaMensagem.scrollIntoView();
     }
-
 }
 
 function enviarMensagem(){
-    const mensagem = document.querySelector('.message-box').value
+    const mensagem = document.querySelector('.message-box').value;
+    const mensagemSemEspaco = mensagem.replace(/ /g, '');
+    if (mensagem ==='' || mensagemSemEspaco === ''){
+        return;
+    }
     const message = {
         from: usuário.name,
 	    to: destinatario,
 	    text: mensagem,
 	    type: tipo 
     }
-    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', message)
-    const input = document.querySelector('.message-box')
-    input.value = ''
-    promise.then(recebeMensagens)
-    promise.catch(mensagemErro)
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', message);
+    const input = document.querySelector('.message-box');
+    input.value = '';
+    promise.then(recebeMensagens);
+    promise.catch(mensagemErro);
 }
 
 function mensagemErro(erro){
-    console.log("Status code: " + erro.response.status)
-    alert('O usuário não se encontra mais na sala.')
-    window.location.reload()
+    console.log("Status code: " + erro.response.status);
+    alert('O usuário não se encontra mais na sala.');
+    window.location.reload();
 }
 
 function showMenu(){
-    const background = document.querySelector('.side_menu_background')
+    const background = document.querySelector('.side_menu_background');
     background.style.display = 'flex';
     const lateral = document.querySelector('.side_menu')
-    lateral.style.visibility = 'visible'
+    lateral.style.visibility = 'visible';
 }
 function hideMenu(){
-    const background = document.querySelector('.side_menu_background')
+    const background = document.querySelector('.side_menu_background');
     background.style.display = 'none';
     const lateral = document.querySelector('.side_menu')
-    lateral.style.visibility = 'hidden'
-}
+    lateral.style.visibility = 'hidden';
+} 
+
 
 function pegaParticipantes(){
-    const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants')
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
     promessa.then((resposta)=> {
         participantes = resposta.data;
-        renderizarParticipantes()
+        renderizarParticipantes();
     })
 }
 function renderizarParticipantes(){
-    const lista = document.querySelector('.online_users')
-    lista.innerHTML = ''
+    const lista = document.querySelector('.online_users');
+    lista.innerHTML = '';
     for(let i = 0; i< participantes.length; i++){
 
         if(participantes[i].name !== usuário.name){
-            lista.innerHTML += 
+            lista.innerHTML +=
             `<div id="${participantes[i].name}" class="participante" onclick="selParticipante(this)">
             <ion-icon name="person-circle"></ion-icon>
             <p>${participantes[i].name}</p>
-            </div>`
-
+            </div>`;
         }
     }
     if(destinatario !== "Todos"){
-        const aa = document.getElementById(destinatario) 
-        if(aa !== null){
-            aa.classList.add('selected')
+        const to = document.getElementById(destinatario);
+        if(to !== null){
+            to.classList.add('selected');
             const divIcon = criarIcone('ícone');
-            aa.appendChild(divIcon)
+            to.appendChild(divIcon);
         } else{
-            selTodos()
+            selTodos();
         }
-        
     }
 }
 
 function criarIcone(id){
     const elDiv = document.createElement('div');
-    elDiv.className = 'icon'
-    elDiv.id = id
-    elDiv.innerHTML = '<ion-icon name="checkmark"></ion-icon>'
-    return elDiv
+    elDiv.className = 'icon';
+    elDiv.id = id;
+    elDiv.innerHTML = '<ion-icon name="checkmark"></ion-icon>';
+    return elDiv;
 }
 
 function selTodos(){
-    const todos = document.getElementById('todos')
-    todos.classList.add('selected')
-    const divIcon = criarIcone('ícone')
-    todos.appendChild(divIcon)
-    destinatario = "Todos"
-    selPublic()
+    const todos = document.getElementById('todos');
+    todos.classList.add('selected');
+    const divIcon = criarIcone('ícone');
+    todos.appendChild(divIcon);
+    destinatario = "Todos";
+    selPublic();
 }
 
 function selPublic(){
     if (tipo === "private_message"){
-        const private = document.getElementById('private_message')
-        private.classList.remove('selected')
-        let removeIcon = document.getElementById('ícone2')
-        removeIcon.parentNode.removeChild(removeIcon)
+        const private = document.getElementById('private_message');
+        private.classList.remove('selected');
+        const removeIcon = document.getElementById('ícone2');
+        removeIcon.parentNode.removeChild(removeIcon);
     }
-    
-    const public = document.getElementById('public')
-    public.classList.add('selected')
-    const divIconVisibility = criarIcone('ícone2')
-    public.appendChild(divIconVisibility)
-    tipo = "message"
-    const legenda = document.getElementById('legenda')
-    legenda.innerHTML = `Enviando para ${destinatario}`
+    const public = document.getElementById('public');
+    public.classList.add('selected');
+    const divIconVisibility = criarIcone('ícone2');
+    public.appendChild(divIconVisibility);
+    tipo = "message";
+    const legenda = document.getElementById('legenda');
+    legenda.innerHTML = `Enviando para ${destinatario} (publicamente)`;
 }
 
 function selParticipante(participante){
-    const selAnterior = document.querySelector('.listaParticipantes .selected')
-    const legenda = document.getElementById('legenda')
+    const selAnterior = document.querySelector('.listaParticipantes .selected');
+    const legenda = document.getElementById('legenda');
 
     if (selAnterior !== null){
-        selAnterior.classList.remove('selected')
-        let removeIcon = document.getElementById('ícone')
-        removeIcon.parentNode.removeChild(removeIcon)
+        selAnterior.classList.remove('selected');
+        const removeIcon = document.getElementById('ícone');
+        removeIcon.parentNode.removeChild(removeIcon);
     }
 
-    participante.classList.add('selected')
+    participante.classList.add('selected');
     const divIcon = criarIcone('ícone');
-    participante.appendChild(divIcon)
-    const selecionado = document.querySelector('.listaParticipantes .selected p')
-    destinatario = selecionado.innerHTML
-    if (tipo === 'private_message'){
-        legenda.innerHTML = `Enviando para ${destinatario} (reservadamente)`
-    } else {
-        legenda.innerHTML = `Enviando para ${destinatario}`
+    participante.appendChild(divIcon);
+    const selecionado = document.querySelector('.listaParticipantes .selected p');
+    destinatario = selecionado.innerHTML;
+    if(destinatario ==='Todos'){
+        if (tipo ==='private_message'){
+            selPublic();
+        }
     }
-
+    if (tipo === 'private_message'){
+        legenda.innerHTML = `Enviando para ${destinatario} (reservadamente)`;
+    } else {
+        legenda.innerHTML = `Enviando para ${destinatario} (publicamente)`;
+    }
 }
 
 function selVisibilidade(visibilidade){
-    const legenda = document.getElementById('legenda')
+    const legenda = document.getElementById('legenda');
     if (destinatario === 'Todos'){
-        console.log('aaaa')
         if(visibilidade.id === 'private_message'){
-            return
+            return;
         }
     }
     
-    const selAnteriorVis = document.querySelector('.options_visibility .selected')
+    const selAnteriorVis = document.querySelector('.options_visibility .selected');
     if (selAnteriorVis !== null){
-        selAnteriorVis.classList.remove('selected')
-        let removeIcon = document.getElementById('ícone2')
-        removeIcon.parentNode.removeChild(removeIcon)
+        selAnteriorVis.classList.remove('selected');
+        const removeIcon = document.getElementById('ícone2');
+        removeIcon.parentNode.removeChild(removeIcon);
     }
-    visibilidade.classList.add('selected')
+    visibilidade.classList.add('selected');
     const divIcon = criarIcone('ícone2');
-    visibilidade.appendChild(divIcon)
+    visibilidade.appendChild(divIcon);
     if (visibilidade.id === 'private_message'){
-        tipo = 'private_message'
-        legenda.innerHTML = `Enviando para ${destinatario} (reservadamente)`
+        tipo = 'private_message';
+        legenda.innerHTML = `Enviando para ${destinatario} (reservadamente)`;
     } else {
-        tipo = 'message'
-        legenda.innerHTML = `Enviando para ${destinatario}`
+        tipo = 'message';
+        legenda.innerHTML = `Enviando para ${destinatario} (publicamente)`;
     }
 }
